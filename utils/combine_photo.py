a1b79c7c875230ab3a3a3742f2b46dd4437da7a2
ui0618 <69187130+ui0618@users.noreply.github.com>
2023-09-01 08:30:37 +0900

    combine photo 수정

--- utils/combine_photo.py
+++ utils/combine_photo.py
@@ -2,31 +2,36 @@ from PIL import Image
 import os
 
 
-background_path = os.path.join(os.getcwd(),"잠신제 인생네컷","combine","background.png")
-photo_path = [os.path.join(os.getcwd(),"잠신제 인생네컷","combine","photo"+str(num)+".png") for num in range(1,5)]
-result_path = os.path.join(os.getcwd(),"잠신제 인생네컷","combine","result.png")
+def combine_photo():
+    background_path = os.path.join(os.getcwd(),"clientapp","static","images","2x3_white.png")
+    photo_path = [os.path.join(os.getcwd(),"img_"+str(num)+".png") for num in range(1,7)]
+    result_path = os.path.join(os.getcwd(),"result.png")
 
-background = Image.open(background_path)
-photo = []
-for path in photo_path:
-    photo.append(Image.open(path))
+    background = Image.open(background_path)
+    photo = []
+    for path in photo_path:
+        photo.append(Image.open(path))
 
-photo_width = int(0.4 * background.size[0])
-middle_width = int(0.08 * background.size[0])
-left_margin = int(0.06 * background.size[0])
-right_margin = int(left_margin)
+    middle_width = int(0.02 * background.size[0])
+    middle_width2 = int(0.02 * background.size[0])
+    left_margin = int(0.04 * background.size[0])
+    right_margin = int(left_margin)
+    photo_width = int(0.92 * background.size[0] / 3)
 
-photo_height = int(0.4 * background.size[0])
-middle_height = int(middle_width)
-top_margin = int(left_margin)
-below_margin = int(background.size[1] - (photo_height*2 + middle_height + top_margin))
+    photo_height = int(photo_width * photo.size[1] / photo.size[0])
+    middle_height = int(0.02 * background.size[0])
+    top_margin = int(0.05 * background.size[0])
+    below_margin = int(background.size[1] - (photo_height*2 + middle_height + top_margin))
 
-for i in range(4):
-    photo[i] = photo[i].resize((photo_width, photo_height))
+    for i in range(4):
+        photo[i] = photo[i].resize((photo_width, photo_height))
 
-background.paste(photo[0], (left_margin,top_margin))
-background.paste(photo[1], (left_margin+photo_width+middle_width,top_margin))
-background.paste(photo[2], (left_margin,top_margin+photo_height+middle_height))
-background.paste(photo[3], (left_margin+photo_width+middle_width,top_margin+photo_height+middle_height))
+    background.paste(photo[0], (left_margin,top_margin))
+    background.paste(photo[1], (left_margin+photo_width+middle_width,top_margin))
+    background.paste(photo[2], (left_margin+photo_width*2+middle_width*2,top_margin))
 
-background.save(result_path)
+    background.paste(photo[3], (left_margin,top_margin+photo_height+middle_height))
+    background.paste(photo[4], (left_margin+photo_width+middle_width,top_margin+photo_height+middle_height))
+    background.paste(photo[5], (left_margin+photo_width*2+middle_width*2,top_margin+photo_height+middle_height))
+
+    background.save(result_path)
